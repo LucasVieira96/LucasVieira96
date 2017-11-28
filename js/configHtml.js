fbef13e5f589dde08fc301c080231b99b9a16973
lucasvieira96 <lucas.vieira@edu.univali.br>
2017-11-28 16:52:45 -0200

    melhora na estrutura js e configurações de js no index

---
+++ js/configHtml.js
@@ -0,0 +1,8 @@
+//FOCO
+$(document).ready(function () {
+    $('#first').focus();
+    $('input#input_text, textarea#textarea1').characterCounter();
+});
+
+//menuLateral
+$(".button-collapse").sideNav();
