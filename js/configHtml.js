1ed6f755ef555416b128731795d5851b071fcfef
lucasvieira96 <lucas.vieira@edu.univali.br>
2017-12-20 16:15:58 -0200

    primefaces

--- js/configHtml.js
+++ js/configHtml.js
@@ -3,7 +3,49 @@ $(document).ready(function () {
     $('#first').focus();
     $('input#input_text, textarea#textarea1').characterCounter();
     $('.modal').modal();
+    $('.tooltipped').tooltip({delay: 50});
 });
 
 //menuLateral
 $(".button-collapse").sideNav();
+
+function filtroSolicitante(campo) {
+    // Declare variables 
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("searchSolicitante");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableChamados");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[campo];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      } 
+    }
+  }
+function filtroSetor(campo) {
+    // Declare variables 
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("searchSetor");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableChamados");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[campo];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      } 
+    }
+  }
